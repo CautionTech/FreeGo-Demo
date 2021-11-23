43c148882d83220d35a1ae12212f4bcb5769501d
kingyeet9048 <sulaimanbada8048@gmail.com>
2021-11-22 18:08:21 -0600

    test fixed

---
+++ src/tests/core.test.js
@@ -0,0 +1,13 @@
+import { shallow } from "enzyme";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './../redux/store';
+import App from './../components/App/App';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+it("Renders without crashing", () => {
+    shallow(<Provider store={store}><App /></Provider>);
+});
